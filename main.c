ce71a260682af1d009151d8d807923947e233df7
sumu <2038035593@qq.com>
2026-06-02 12:08:19 +0000

    feat: 添加my-psh代码

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "src/func_base64.h"
+#include "src/func_qrencode.h"
+int main(int argc, char **argv) {
+    const char *text;
+
+    if (argc < 2) {
+        text = "Hello, QR Code!";
+    }
+    else {
+        text = argv[1];
+    }
+
+    /* ========== QR Code 打印 ========= */
+    printf("Text: %s\n\n", text);
+    func_qrencode_generate_and_print(text);
+    func_base64_test();
+    printf("\nDone.\n");
+    return 0;
+}
